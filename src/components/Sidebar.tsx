ff250a1af158ca84bc4b564c35a3478c5ef3fd31
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-08-25 14:04:28 +0100

    refactor: Update Sidebar component to include missing submenu for Instructors

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -27,24 +27,20 @@ const Sidebar: React.FC<SidebarProps> = ({ mobile = false }) => {
             <Menu.Item key="/iadmin/dashboard" icon={<DashboardOutlined />}>
                 <Link to="/iadmin/dashboard">Dashboard</Link>
             </Menu.Item>
-            <Menu.SubMenu
-                key="sub1"
-                icon={<UserOutlined />}
-                title="Instructors"
-            >
-                <Menu.Item key="/iadmin/instructors/1">
-                    <Link to="/iadmin/instructors/1">Professor 1</Link>
-                </Menu.Item>
-                <Menu.Item key="/iadmin/instructors/2">
-                    <Link to="/iadmin/instructors/2">Professor 2</Link>
-                </Menu.Item>
-            </Menu.SubMenu>
             <Menu.SubMenu key="sub2" icon={<BookOutlined />} title="Courses">
                 <Menu.Item key="/iadmin/courses">
                     <Link to="/iadmin/courses">All Courses</Link>
                 </Menu.Item>
                 <Menu.Item key="/iadmin/courses/add">
                     <Link to="/iadmin/courses/add">Add Course</Link>
+                </Menu.Item>
+            </Menu.SubMenu>
+            <Menu.SubMenu key="sub1" icon={<UserOutlined />} title="Instructors">
+                <Menu.Item key="/iadmin/instructors/1">
+                    <Link to="/iadmin/instructors/1">Professor 1</Link>
+                </Menu.Item>
+                <Menu.Item key="/iadmin/instructors/2">
+                    <Link to="/iadmin/instructors/2">Professor 2</Link>
                 </Menu.Item>
             </Menu.SubMenu>
             <Menu.Item key="/iadmin/students" icon={<FileTextOutlined />}>
