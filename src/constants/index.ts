7cb226207f35c0c536af58b68607d88143c7fa7e
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-08-27 14:33:56 +0100

    refactor: Add CLOUDINARY_CLOUD_NAME constant to index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,5 @@
 export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 console.log('SERVER_URL:', SERVER_URL);
+
+export const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+console.log('CLOUDINARY_CLOUD_NAME:', CLOUDINARY_CLOUD_NAME);
