a196be668acb4f34e077efb4e3b411517bf36ada
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-08-28 13:27:47 +0100

    refactor: Update Sidebar component to display admin management option for super admins

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,10 @@
 // components/Sidebar.tsx
 import React from 'react';
 import { Menu } from 'antd';
-import {
-    DashboardOutlined,
-    UserOutlined,
-    BookOutlined,
-    FileTextOutlined,
-} from '@ant-design/icons';
+import { DashboardOutlined, UserOutlined, BookOutlined, TeamOutlined, SettingOutlined } from '@ant-design/icons';
 import { Link, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { RootState } from '../state/store';
 
 interface SidebarProps {
     mobile?: boolean;
@@ -15,15 +12,10 @@ interface SidebarProps {
 
 const Sidebar: React.FC<SidebarProps> = ({ mobile = false }) => {
     const location = useLocation();
+    const currentAdmin = useSelector((state: RootState) => state.auth.admin);
 
     return (
-        <Menu
-            defaultSelectedKeys={[location.pathname]}
-            // defaultOpenKeys={['sub1', 'sub2']}
-            mode="inline"
-            theme="light"
-            className={`h-full ${mobile ? 'px-2 py-4' : ''}`}
-        >
+        <Menu defaultSelectedKeys={[location.pathname]} mode="inline" theme="light" className={`h-full ${mobile ? 'px-2 py-4' : ''}`}>
             <Menu.Item key="/iadmin/dashboard" icon={<DashboardOutlined />}>
                 <Link to="/iadmin/dashboard">Dashboard</Link>
             </Menu.Item>
@@ -35,17 +27,17 @@ const Sidebar: React.FC<SidebarProps> = ({ mobile = false }) => {
                     <Link to="/iadmin/courses/add">Add Course</Link>
                 </Menu.Item>
             </Menu.SubMenu>
-            <Menu.SubMenu key="sub1" icon={<UserOutlined />} title="Instructors">
-                <Menu.Item key="/iadmin/instructors/1">
-                    <Link to="/iadmin/instructors/1">Professor 1</Link>
-                </Menu.Item>
-                <Menu.Item key="/iadmin/instructors/2">
-                    <Link to="/iadmin/instructors/2">Professor 2</Link>
-                </Menu.Item>
-            </Menu.SubMenu>
-            <Menu.Item key="/iadmin/students" icon={<FileTextOutlined />}>
+            <Menu.Item key="/iadmin/instructors" icon={<UserOutlined />}>
+                <Link to="/iadmin/instructors">Instructors</Link>
+            </Menu.Item>
+            <Menu.Item key="/iadmin/students" icon={<TeamOutlined />}>
                 <Link to="/iadmin/students">Students</Link>
             </Menu.Item>
+            {currentAdmin?.isSuperAdmin && (
+                <Menu.Item key="/iadmin/admins" icon={<SettingOutlined />}>
+                    <Link to="/iadmin/admins">Admin Management</Link>
+                </Menu.Item>
+            )}
         </Menu>
     );
 };
